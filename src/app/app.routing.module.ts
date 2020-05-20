dc3e048756db5cf1fb97471753683d88fa12c9b8
Ebere <ebereuzodufa@gmail.com>
2020-05-20 02:38:31 +0100

    Add routes

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HistoryComponent } from './history/history.component';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'history',
+    component: HistoryComponent
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
